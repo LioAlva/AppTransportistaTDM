84c4561df318363e46f824c5615a306823f6fd8e
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-11-27 13:36:20 -0500

    05 Configurado Login

---
+++ SolucionTransportistaTDM/AppTransportistaTDM/AppTransportistaTDM/ViewModels/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransportistaTDM.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string User { get; set; }
+        public string Password { get; set; }
+        public bool IsRemembered { get; set; }
+        //public bool IsRunning //servira para el activity indicator 
+        //{
+        //    set
+        //    {
+        //        if (isRunning != value)
+        //        {
+        //            isRunning = value;
+        //            /* if (PropertyChanged!=null) {
+        //                 PropertyChanged(this, new PropertyChangedEventArgs("IsRunning"));
+        //             }*///es lo mismo que lo de abajo
+        //            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+        //        }
+        //    }
+        //    get
+        //    {
+        //        return isRunning;
+        //    }
+        //}
+        #endregion
+    }
+}
--- SolucionTransportistaTDM/AppTransportistaTDM/AppTransportistaTDM/ViewModels/MainViewModel.cs
+++ SolucionTransportistaTDM/AppTransportistaTDM/AppTransportistaTDM/ViewModels/MainViewModel.cs
@@ -9,18 +9,23 @@ namespace AppTransportistaTDM.ViewModels
 {
     public  class MainViewModel
     {
+        #region Properties
         public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        public LoginViewModel NewLogin { get; set; }
+        //public  NewLogin 
+        #endregion
 
         public MainViewModel()
         {
             LoadMenu();
+            //Create Views
+            NewLogin = new LoginViewModel();
         }
 
         private void LoadMenu()
         {
             Menu = new ObservableCollection<MenuItemViewModel>();
-
-
         }
 
     }
