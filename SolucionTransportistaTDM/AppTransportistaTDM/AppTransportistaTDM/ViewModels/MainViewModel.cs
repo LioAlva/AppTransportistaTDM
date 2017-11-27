a5c8d76756b2151d5f01a3a8c311c1a906634e07
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-11-27 13:17:55 -0500

    02 MASTERVIEW Funcionando con InstanceLocator

---
+++ SolucionTransportistaTDM/AppTransportistaTDM/AppTransportistaTDM/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTransportistaTDM.ViewModels
+{
+    public  class MainViewModel
+    {
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+
+        public MainViewModel()
+        {
+            LoadMenu();
+        }
+
+        private void LoadMenu()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+
+
+        }
+
+    }
+}
